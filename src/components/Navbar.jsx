7726019169f0d02bded1996c3d14d3218afa1d7c
unknown <sayyidmahaboob@outlook.com>
2023-05-07 18:46:21 +0530

    favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 //dependencies
 
 import React, { useState } from "react";
-import Logo from "..src/assets/pizzaLogo.png";
+// import Logo from "..src/assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
 import MenuTwoToneIcon from "@mui/icons-material/MenuTwoTone";
 
@@ -14,7 +14,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src={"..src/assets/pizzaLogo.png"} />
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu">Menu</Link>
